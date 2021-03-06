01ace826d912019a3dabe210c795a706cd4d13d9
C. Gremaud <colyn.gremaud@gmail.com>
2021-03-06 11:16:32 -0600

    added window.confirm to script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 window.addEventListener("load", () =>{
     let modeSwitchButton = document.getElementById("dark-mode-select")
     modeSwitchButton.addEventListener("click", () =>{
-        window.confirm("are you sure you want to switch to light mode?")
+        let confirm = window.confirm("are you sure you want to switch to light mode?")
+        if (confirm){
+            modeSwitchButton.innerHTML = "Click to activate dark mode"
+            // document.body.style.backgroundColor = "white"
+        }
     })
 })
